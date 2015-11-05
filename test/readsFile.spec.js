a907aa9b234dd031b1380f4e9b03ad837d655948
Dean Radcliffe <deanmisc@yahoo.com>
2015-11-05 15:54:37 -0600

    Ensure array of strings is returned from ReadsFile

--- test/readsFile.spec.js
+++ test/readsFile.spec.js
@@ -4,12 +4,13 @@ let subject = require('../lib/readsFile')
 
 describe('ReadsFile', () => {
   describe('#read', () => {
-    it ('should returns the contents of the file', () => {
+    it ('should returns the contents of the file as an array', () => {
       let filePath = getFixturePath('dummyData.txt')
 
       let result = subject.read(filePath)
 
-      expect(result).to.equal('foo\n')
+      expect(result).to.be.instanceof(Array)
+      expect(result[0]).to.equal('foo')
     })
   })
 })
