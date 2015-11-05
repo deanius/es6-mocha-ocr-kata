59ebca1cdf8a655e62771f9e3180176e9f58eeee
Dean Radcliffe <deanmisc@yahoo.com>
2015-11-05 14:38:27 -0600

    update deps and attempt to use 'testdouble' lib

--- test/readsAccountNumbers.spec.js
+++ test/readsAccountNumbers.spec.js
@@ -1,6 +1,8 @@
 require('./specHelper')
 
-let subject = require('../lib/readsAccountNumbers')
+//let AccountNumber = td.object('AccountNumber')
+
+//let subject = require('../lib/readsAccountNumbers')(AccountNumber)
 
 describe('ReadsAccountNumbers', () => {
   describe('#read', () => {
@@ -10,6 +12,12 @@ describe('ReadsAccountNumbers', () => {
       let result = subject.read(filePath)
 
       expect(result).to.be.instanceof(Array)
+    })
+
+    it ('should return an array of objects that have digits', () => {
+      let result = subject.read(filePath)
+
+      expect(result[0].digits()).to.equal([1,2,3,4,5,6,7,8,9])
     })
   })
 })
